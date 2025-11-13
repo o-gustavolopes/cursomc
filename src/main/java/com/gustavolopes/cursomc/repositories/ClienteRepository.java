4cffb28dfeffdc406cf6a7ad23a4c3edb21f6209
Gustavo Lopes <gustavolopesmartins2506@gmail.com>
2025-11-13 15:03:10 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/gustavolopes/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.gustavolopes.cursomc.repositories;
+
+import com.gustavolopes.cursomc.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/gustavolopes/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.gustavolopes.cursomc.repositories;
+
+import com.gustavolopes.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
